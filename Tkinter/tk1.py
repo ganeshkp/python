031e2d152c1bbc1a1b991058e0c0435b59d3ebb3
ganesh <ganesh.kumar2@ge.com>
2020-03-24 18:01:51 +0000

    Committing modified files

--- Tkinter/tk1.py
+++ Tkinter/tk1.py
@@ -1,9 +1,12 @@
+def callFunc():
+    print("This is ganesh\n")
+
 from tkinter import *
-
-root=Tk()
-
-myLabel=Label(root, text="Hello world!")
-
-myLabel.pack()
-
+root = Tk()
+btn = Button(root, text="Click me!")
+#btn.config(command=lambda: print("Hello, Tkinter!"))
+btn.config(command=callFunc())
+btn.pack(padx=120, pady=30)
+root.title("My Tkinter app")
 root.mainloop()
+

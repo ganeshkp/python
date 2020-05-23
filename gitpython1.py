a1090e05a1c96d122c4b8b40b1818c1965d0b9aa
Ganesh <ganeshkumar.patil@gmail.com>
2020-05-23 21:12:06 +0100

    COmmitting new files

--- gitpython1.py
+++ gitpython1.py
@@ -6,6 +6,7 @@ import git
 GIT_REPO="https://github.com/ganeshkp/apex-read.git"
 GIT_FILDER="C:/WORK/VM_SHARE/apex-read"
 
+#https://stackoverflow.com/questions/13166595/how-can-i-pull-a-remote-repository-with-gitpython
 repo=git.Repo(GIT_FILDER)
 o=repo.remotes.origin
 o.pull()
